cb13b20829462eece6110860e850080a87139c75
Sudharshan <imsudharshan281@gmail.com>
2025-04-17 22:43:56 +0530

    test: Modify API file to trigger documentation update PR

--- src/api/users.py
+++ src/api/users.py
@@ -1,6 +1,6 @@
 # users.py
 
 # Added a new function for testing
-def updated_function():
-    """This is an updated test function to trigger documentation update."""
+def another_updated_function():
+    """This is another test function to trigger documentation update."""
     pass
